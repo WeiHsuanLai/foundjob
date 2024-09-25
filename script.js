d11ab828f54f5c7be8548af99d7bd48b2519ed3c
WeiHsuanLai <hsuan29684@gmail.com>
2024-09-25 22:23:11 +0800

    pronpt() 與 alert()

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // console.log("HI!Js!");
 // 變數宣告 (var const let)
 
-let name = "Leo"
-console.log(name);
+// let name = "Leo"
+// console.log(name);
 
-let sentence = '今天過得如何?很高興見到你，希望你有美好的一天!'
+// let sentence = '今天過得如何?很高興見到你，希望你有美好的一天!'
+// console.log(name + sentence);
 
-console.log(name + sentence);
+// 內建方法介紹 pronpt() 與 alert()
+
+fruit = prompt("你最喜歡的水果是什麼?")
+// console.log(fruit);
+alert(fruit)
