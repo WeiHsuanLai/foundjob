3b8c95e5a1546260fe6be20396248ddbc4478704
WeiHsuanLai <hsuan29684@gmail.com>
2024-09-25 22:19:40 +0800

    設定兩個字串將它們串再一起

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("HI!Js!");
+// console.log("HI!Js!");
+// 變數宣告 (var const let)
+
+let name = "Leo"
+console.log(name);
+
+let sentence = '今天過得如何?很高興見到你，希望你有美好的一天!'
+
+console.log(name + sentence);
